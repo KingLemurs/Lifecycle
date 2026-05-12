5deae8943560111efd64a7d1dc2a661cae0662ac
DasSkippy <sean.frederico105@gmail.com>
2026-05-11 18:32:00 -0700

    added pause menu and transition for playground

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool paused;
+    public GameObject PauseUI;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,32 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+        }
+
+        if (paused)
+            Pause();
+        else
+            Unpause();
+    }
+
+    private void Pause()
+    {
+        paused = true;
+        PauseUI.SetActive(true);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        Time.timeScale = 0f;
+    }
+
+    private void Unpause()
+    {
+        paused = false;
+        PauseUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        Time.timeScale = 1f;
     }
 }
